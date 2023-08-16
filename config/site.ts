47805ea3ea10c2e2ba71f4df73345409cc8eeb18
Lucian <Lucian.io@outlook.com>
2023-08-15 22:53:10 -0400

    init for next ui database

--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,51 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Home",
+      href: "/",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
+      label: "iPads",
+      href: "/ipads",
     },
     {
-      label: "Blog",
-      href: "/blog",
+      label: "Laptops",
+      href: "/laptops",
+    },
+    // {
+    //   label: "iPads (repair)",
+    //   href: "/repair/ipads",
+    // },
+    // {
+    //   label: "Laptops (repair)",
+    //   href: "/repair/laptops",
+    // },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
     },
     {
-      label: "About",
-      href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
-	},
-};
+      label: "iPads",
+      href: "/ipads",
+    },
+    {
+      label: "Laptops",
+      href: "/laptops",
+    },
+    {
+      label: "iPads (repair)",
+      href: "/repair/ipads",
+    },
+    {
+      label: "Laptops (repair)",
+      href: "/repair/laptops",
+    },
+  ],
+
+}
