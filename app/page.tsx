ff270c3f74d83fbd283ceb2f9d11896af56e3bb9
Lucian <Lucian.io@outlook.com>
2023-09-21 00:55:51 -0400

    fix 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Home from '@/components/pages/home'
-import { authOptions } from '@/util/lib/nextAuth/authOptions'
-import GetAllVerifiedUsers from '@/util/server/Users/GetAllVerifiedUsers'
-import { getServerSession } from 'next-auth'
+// import { authOptions } from '@/util/lib/nextAuth/authOptions'
+// import GetAllVerifiedUsers from '@/util/server/Users/GetAllVerifiedUsers'
+// import { getServerSession } from 'next-auth'
 // import GreetingOverlay from '@/components/pages/overlay/greeting-overlay'
 import { getData } from '@/util/server/get-data'
 
 export default async function HomePage() {
-  const session = await getServerSession(authOptions)
-  const verifiedUsers = await GetAllVerifiedUsers()
-  const isUserAuth = verifiedUsers.some(
-    (user) => user.email === session?.user?.email
-  )
+  // const session = await getServerSession(authOptions)
+  // const verifiedUsers = await GetAllVerifiedUsers()
+  // const isUserAuth = verifiedUsers.some(
+  //   (user) => user.email === session?.user?.email
+  // )
 
   // const data = isUserAuth ? await getData(true) : null;
   const data = await getData(true);
